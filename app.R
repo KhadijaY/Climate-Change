26e34847213dfb67c87d666aea7032b2826c8131
KhadijaY <102887672+KhadijaY@users.noreply.github.com>
2022-06-08 22:59:34 -0700

    Add files via upload

--- app.R
+++ app.R
@@ -1,42 +1,42 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
 
 
 library(shiny)
-library(ggplot2)
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Climate Change-Co2 emission trends "),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("emissions",
-                        "Number of Co2 emissions:",
-                        min = 1,
-                        max = 8000,
-                        value = 8000)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("scatterPlot")
-        )
+ui<-fluidPage(
+  titlePanel("Co2 Trends compared to Years"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(inputId = "bins",
+                  label = "number of bins:",
+                  min=1,
+                  max=50,
+                  value=50)
+    ),
+    
+        
+    mainPanel(
+      plotOutput(outputId = "distPlot")
     )
+  )
 )
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+server<-function(input,output){
   Co2_dataset<-read.csv("https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.csv") 
-  
-    output$scatterPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-      ggplot(Co2_dataset,aes(x=year,y=co2))+
-        geom_point()
-
-    })
+  output$distPlot<-renderPlot({
+    x <- Co2_dataset$year
+    y<-Co2_dataset$co2
+    bins<-seq(min(x),max(x),length.out=input$bins + 1)
+    hist(x,breaks=bins,col="#75AADB",border="white",
+         xlab="year",
+         ylab="co2",
+         main="histogram of Co2 levels")
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
